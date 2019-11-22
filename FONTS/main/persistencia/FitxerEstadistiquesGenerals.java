0c171883ec8ffc7cdf1be6391830f2f76f4f4c10
annallanza <annallanza@hotmail.com>
2019-11-22 21:27:55 +0100

    Create FitxerEstadistiquesGenerals.java

---
+++ FONTS/main/persistencia/FitxerEstadistiquesGenerals.java
@@ -0,0 +1,26 @@
+package main.persistencia;
+
+import java.io.File;
+
+
+public class FitxerEstadistiquesGenerals {
+
+    public FitxerEstadistiquesGenerals() {
+    }
+
+    public Object [] getAllEstadistiquesFile() {
+        Object[] AllEstadistiques = new Object[32];
+
+        String path = "";
+
+        File file = new File(path);
+
+
+
+        return AllEstadistiques;
+    }
+
+    public void saveAllEstadistiquesFile(Object[] estadistiques) {
+        //
+    }
+}
