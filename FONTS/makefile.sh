a3c2ac6afd97c95ecdac7a59ae0209bcfcb69eda
andugu <andugu@users.noreply.github.com>
2019-11-25 17:17:30 +0100

    Update makefile.sh

--- FONTS/makefile.sh
+++ FONTS/makefile.sh
@@ -2,10 +2,26 @@
 
 CLASSPATH=:.:test/JUnitJARs/junit-4.10.jar:test/JUnitJARs/hamcrest-core-1.3.jar
 
-javac main/domini/*.java
-javac main/presentacio/*.java
-javac main/persistencia/*.java
 
-javac -cp $CLASSPATH test/domini/*.java
+echo Compresor compiler utility, please select an option:
+echo 1. Compile all
+echo 2. Remove all *.class
+echo I choose:
+read n
 
-jar cfm Compresor.jar Manifest.txt main/domini/*.class main/persistencia/*.class main/presentacio/*.class
+if [ $n == 1 ]; then
+	javac main/domini/*.java
+	javac main/presentacio/*.java
+	javac main/persistencia/*.java
+
+	javac -cp $CLASSPATH test/domini/*.java
+
+	jar cfm Compresor.jar Manifest.txt main/domini/*.class main/persistencia/*.class main/presentacio/*.class
+fi
+
+if [ $n == 2 ]; then
+	rm -rf main/domini/*.class
+	rm -rf main/presentacio/*.class
+	rm -rf main/persistencia/*.class
+	rm -rf test/domini/*.class
+fi
