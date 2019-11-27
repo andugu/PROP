4fa62b7da42fe3c10449caf1c2ab4e1f8810091b
annallanza <annallanza@hotmail.com>
2019-11-27 11:56:10 +0100

    Update FitxerEstadistiquesGenerals.java

--- FONTS/main/persistencia/FitxerEstadistiquesGenerals.java
+++ FONTS/main/persistencia/FitxerEstadistiquesGenerals.java
@@ -1,6 +1,6 @@
 package main.persistencia;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FitxerEstadistiquesGenerals {
@@ -8,19 +8,50 @@ public class FitxerEstadistiquesGenerals {
     public FitxerEstadistiquesGenerals() {
     }
 
-    public Object [] getAllEstadistiquesFile() {
+    public Object [] getAllEstadistiquesFile() throws IOException {
         Object[] AllEstadistiques = new Object[32];
 
         String path = "";
 
-        File file = new File(path);
+        /*
+        Hem de llegir:
+        - grau (double)
+        - velocitat (double)
+        - temps (double)
+        - numeroElements (int)
+        */
+
+        
+        String cadena;
+        FileReader f = new FileReader(path);
+        BufferedReader b = new BufferedReader(f);
+        while((cadena = b.readLine())!=null) {
+            System.out.println(cadena);
+        }
+        b.close();
 
 
 
         return AllEstadistiques;
     }
 
-    public void saveAllEstadistiquesFile(Object[] estadistiques) {
-        //
+    public void saveAllEstadistiquesFile(Object[] estadistiques) throws IOException {
+        String path = "";
+
+        String cadena;
+        FileReader f = new FileReader(path);
+        BufferedReader b = new BufferedReader(f);
+        while((cadena = b.readLine())!=null) {
+            System.out.println(cadena);
+        }
+        b.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        FitxerEstadistiquesGenerals fEstGen = new FitxerEstadistiquesGenerals();
+
+        Object[] estadistiques = fEstGen.getAllEstadistiquesFile();
+
+        fEstGen.saveAllEstadistiquesFile(estadistiques);
     }
 }
