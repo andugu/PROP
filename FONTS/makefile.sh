47777a2645b7731f7f3397ca0074aef57d35c308
andugu <andugu@users.noreply.github.com>
2019-12-05 15:10:35 +0100

    It just works™

--- FONTS/makefile.sh
+++ FONTS/makefile.sh
@@ -5,7 +5,7 @@ CLASSPATH=:.:test/JUnitJARs/junit-4.10.jar:test/JUnitJARs/hamcrest-core-1.3.jar
 
 echo Compresor compiler utility, please select an option:
 echo 1. Compile all
-echo 2. Remove all *.class
+echo 2. Remove all *.class and *.jar
 echo I choose:
 read n
 
@@ -13,14 +13,12 @@ if [ $n == 1 ]; then
 	javac main/domini/*.java
 	javac main/presentacio/*.java
 	javac main/persistencia/*.java
-
 	javac -cp $CLASSPATH test/domini/*.java
 
 	jar cfm Compresor.jar Manifest.txt main/domini/*.class main/persistencia/*.class main/presentacio/*.class
 fi
 
 if [ $n == 2 ]; then
-	rm -rf ./*.class
 	rm -rf ./main/domini/*.class
 	rm -rf ./main/persistencia/*.class
 	rm -rf ./main/presentacio/*.class
