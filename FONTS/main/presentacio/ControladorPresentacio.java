f591184cc26a634e3f45e34600b6b95acb55772c
andugu <andugu@users.noreply.github.com>
2019-12-05 16:09:58 +0100

    Upd Presentacio

--- FONTS/main/presentacio/ControladorPresentacio.java
+++ FONTS/main/presentacio/ControladorPresentacio.java
@@ -16,47 +16,51 @@ public class ControladorPresentacio {
             System.out.println("Selecciona la funció que vols fer");
             System.out.println("1 - Comprimir");
             System.out.println("2 - Descomprimir");
-            System.out.println("3 - Visualitzar Estadistiques");
-            System.out.println("4 - Comparar");
-            System.out.println("5 - Usage");
-            System.out.println("6 - Sortir");
+            System.out.println("3 - Comprimir Carpeta");
+            System.out.println("4 - Descomprimir Carpeta");
+            System.out.println("5 - Visualitzar Estadistiques");
+            System.out.println("6 - Comparar");
+            System.out.println("7 - Usage");
+            System.out.println("8 - Sortir");
 
 
             Scanner in = new Scanner(System.in);
             int opcio = in.nextInt();
 
-            while (opcio != 1 && opcio != 2 && opcio != 3 && opcio != 4 && opcio != 5 && opcio != 6) {
+            while (opcio < 1 || opcio > 8) {
                 System.out.println("La funció que has escollit és erronia, torna a escriure-la: ");
                 opcio = in.nextInt();
             }
 
             String path;
-            String nomalgorisme;
+            String nomalgorisme = "LZSS";
 
-            if (opcio == 1 || opcio == 2 || opcio == 4) {
+            if ((opcio > 0 && opcio < 5) || opcio == 6) {
                 System.out.println("Introdueix el path: ");
                 path = in.next();
 
-                String[] nomsAlgorisme = cDom.triaAlgorisme(path, opcio);
+                if (opcio != 4){
+	                String[] nomsAlgorisme = cDom.triaAlgorisme(path, opcio);
 
-                System.out.println("Selecciona l'algorisme que vols utilitzar:");
-                for (int i = 0; i < nomsAlgorisme.length; ++i) {
-                    System.out.println(nomsAlgorisme[i]);
-                }
+	                System.out.println("Selecciona l'algorisme que vols utilitzar:");
+	                for (int i = 0; i < nomsAlgorisme.length; ++i) {
+	                    System.out.println(nomsAlgorisme[i]);
+	                }
 
-                nomalgorisme = in.next();
+	                nomalgorisme = in.next();
 
-                boolean correcte;
-                if (nomalgorisme.equals("LZ78") || nomalgorisme.equals("LZSS") || nomalgorisme.equals("LZW") || nomalgorisme.equals("JPEG"))
-                    correcte = true;
-                else correcte = false;
+	                boolean correcte;
+	                if (nomalgorisme.equals("LZ78") || nomalgorisme.equals("LZSS") || nomalgorisme.equals("LZW") || nomalgorisme.equals("JPEG") || nomalgorisme.equals("Automatic"))
+	                    correcte = true;
+	                else correcte = false;
 
-                while (!correcte) {
-                    System.out.print("No és un algorisme correcte. Torna a escriure el nom de l'algorisme, aquest ha de ser LZ78 o LZSS o LZW o JPEG: ");
-                    nomalgorisme = in.next();
-                    if (nomalgorisme.equals("LZ78") || nomalgorisme.equals("LZSS") || nomalgorisme.equals("LZW") || nomalgorisme.equals("JPEG"))
-                        correcte = true;
-                }
+	                while (!correcte) {
+	                    System.out.print("No és un algorisme correcte. Torna a escriure el nom de l'algorisme, aquest ha de ser LZ78 o LZSS o LZW o JPEG o Automatic: ");
+	                    nomalgorisme = in.next();
+	                    if (nomalgorisme.equals("LZ78") || nomalgorisme.equals("LZSS") || nomalgorisme.equals("LZW") || nomalgorisme.equals("JPEG") || nomalgorisme.equals("Automatic"))
+	                        correcte = true;
+	                }
+	            }
 
                 if (opcio == 1) {
 
@@ -72,7 +76,13 @@ public class ControladorPresentacio {
                     System.out.println("El grau és: " + (double) estgen[0]);
                     System.out.println("La velocitat és: " + (double) estgen[1]);
                     System.out.println("El temps és: " + (long) estgen[2]);
-                } else { //opcio 4
+
+                } else if (opcio == 3){
+                	cDom.comprimirCarpeta(path, nomalgorisme);
+
+               	} else if (opcio == 4){
+               		cDom.descomprimirCarpeta(path);
+                } else { //opcio 6
                     cDom.comprimir(path, nomalgorisme);
 
                     String nou_path = path + '.' + nomalgorisme;
@@ -93,7 +103,7 @@ public class ControladorPresentacio {
                 }
             }
 
-            if (opcio == 3) {
+            if (opcio == 5) {
                 System.out.println("Selecciona l'algorisme que vols utilitzar:");
                 System.out.println("LZ78 ");
                 System.out.println("LZSS ");
@@ -135,18 +145,20 @@ public class ControladorPresentacio {
                 System.out.println("La velocitat mitjana és: " + (double) estgen[1]);
                 System.out.println("El temps mitjà és: " + (double) estgen[2]);
             }
-            else if(opcio == 5) {
+            else if(opcio == 7) {
                 System.out.println("Manual d'usuari: ");
                 System.out.println("");
-                System.out.println("Amb la opcio 1 - Comprimit, es comprimeix un arxiu introduit per l'usuari, amb l'algorisme especificat");
+                System.out.println("Amb la opcio 1 - Comprimir, es comprimeix un arxiu introduit per l'usuari, amb l'algorisme especificat");
                 System.out.println("Amb la opcio 2 - Descomprimir, es descomprimeix un arxiu introduit per l'usuari, amb l'algorisme amb el que es va comprimir");
-                System.out.println("Amb la opcio 3 - Visualitzar Estadistiques, es mostren per pantalla les estadistiques globals del programa");
-                System.out.println("Amb la opcio 4 - Comparar, es comprimeix un arxiu introduit per l'usuari, amb l'algorisme especificat, i seguidament es descomprimeix, a continuació s'obren ambdos per poder veure'n les diferències");
-                System.out.println("Amb la opcio 5 - Usage, es mostra aquest missatge per pantalla");
-                System.out.println("Amb la opcio 6 - Sortir, es tanca el programa");
+                System.out.println("Amb la opcio 3 - Comprimir Carpeta, es comprimeix una carpeta introduida per l'usuari, amb l'algorisme especificat");
+                System.out.println("Amb la opcio 4 - Descomprimir Carpeta, es descomprimeix una carpeta introduida per l'usuari, amb l'algorisme amb el que es va comprimir");
+                System.out.println("Amb la opcio 5 - Visualitzar Estadistiques, es mostren per pantalla les estadistiques globals del programa");
+                System.out.println("Amb la opcio 6 - Comparar, es comprimeix un arxiu introduit per l'usuari, amb l'algorisme especificat, i seguidament es descomprimeix, a continuació s'obren ambdos per poder veure'n les diferències");
+                System.out.println("Amb la opcio 7 - Usage, es mostra aquest missatge per pantalla");
+                System.out.println("Amb la opcio 8 - Sortir, es tanca el programa");
                 System.out.println("");
             }
-            else if (opcio == 6) {
+            else if (opcio == 8) {
                 cDom.saveAllEstadistiques();
                 System.exit(-1);
             }
