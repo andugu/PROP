0b27042e02a757713cafe60ba02480ba55135dd7
andugu <andugu@users.noreply.github.com>
2019-12-20 19:14:40 +0100

    Update makefile.sh

--- FONTS/makefile.sh
+++ FONTS/makefile.sh
@@ -2,20 +2,28 @@
 
 CLASSPATH=:.:test/JUnitJARs/junit-4.10.jar:test/JUnitJARs/hamcrest-core-1.3.jar
 
-
-echo Compresor compiler utility, please select an option:
-echo 1. Compile all
-echo 2. Remove all *.class and *.jar
-echo I choose:
+echo ""
+echo "##############################"
+echo "# Compressor Compiler Utility #"
+echo "##############################"
+echo ""
+echo "Please select an option:"
+echo ""
+echo "1. Compile all & make .jar exec"
+echo "2. Remove all *.class and *.jar"
+echo ""
+echo "I choose:"
+echo ""
 read n
+echo ""
 
 if [ $n == 1 ]; then
-	javac main/domini/*.java
-	javac main/presentacio/*.java
-	javac main/persistencia/*.java
-	javac -cp $CLASSPATH test/domini/*.java
+	javac -source 8 -target 8 -nowarn main/domini/*.java
+	javac -source 8 -target 8 -nowarn main/presentacio/*.java
+	javac -source 8 -target 8 -nowarn main/persistencia/*.java
+	javac -cp $CLASSPATH -source 8 -target 8 -nowarn test/domini/*.java
 
-	jar cfm Compresor.jar Manifest.txt main/domini/*.class main/persistencia/*.class main/presentacio/*.class
+	jar cfm Compressor.jar Manifest.txt main/domini/*.class main/persistencia/*.class main/presentacio/*.class
 fi
 
 if [ $n == 2 ]; then
@@ -25,3 +33,7 @@ if [ $n == 2 ]; then
 	rm -rf ./test/domini/*.class
 	rm -rf ./*.jar
 fi
+
+echo ""
+echo "Done, enjoy!"
+echo ""
