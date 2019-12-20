54715244e97396ba7fbe71169a98a66e3cf51dc1
annallanza <annallanza@hotmail.com>
2019-12-20 16:24:54 +0100

    Update Descomprimit.java

--- FONTS/main/domini/Descomprimit.java
+++ FONTS/main/domini/Descomprimit.java
@@ -15,6 +15,8 @@ public class Descomprimit extends Fitxer{
         int tamanyNou = contingut_descomprimit.length;
         long temps = (long) contingutItemps[1];
 
+        if(temps == 0) temps = (long) 0.5;
+
         Object[] grauIvelocitat = calcularEstadistiques(tamanyNou, temps);
         Object[] ret = new Object[4];
         
